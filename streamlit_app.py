bb4eb3513a1a2503307ac27804bec49a6b1a502f
tourdupoule <165513907+tourdupoule@users.noreply.github.com>
2024-03-30 21:39:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,9 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect('Choose up to 5 ingredients',my_dataframe,max_selections=5)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
 if ingredients_list:
     # st.write(ingredients_list)
     # st.text(ingredients_list)
